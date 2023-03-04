e47b69664eb22fa40698ccbe2ec08917bf001cbb
AbdulRahmanFatouhe <123048274+AbdulRahmanFatouhe@users.noreply.github.com>
2023-03-03 21:52:53 -0500

    Update Game.java

--- SYST17796_ProjectStarterCode/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Game.java
+++ SYST17796_ProjectStarterCode/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Game.java
@@ -1,48 +1,58 @@
-/**
- * SYST 17796 Project Base code.
+/*
+ *SYST 17796 Project.
  
- * Add your name as an author and the date!
+ * the author of this class : Sara AbuLehyeh
  */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 
+public class Game {
+    public static void main(String[] args) {
+        ArrayList<Card> deck = new ArrayList<>();
 
-public abstract class Game {
+        String[] colors = {"Red", "Yellow", "Green", "Blue"};
+        String[] values = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw Two"};
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+        // Generate the deck
+        for (String color : colors) {
+            for (String value : values) {
+                deck.add(new Card(color, value));
+                if (!value.equals("0")) {
+                    deck.add(new Card(color, value));
+                }
+            }
+        }
 
-    public Game(String name) {
-        this.name = name;
-        players = new ArrayList();
+        // Add the wild cards
+        for (int i = 0; i < 4; i++) {
+            deck.add(new Card("Wild", "Wild Card"));
+            deck.add(new Card("Wild", "Draw Four"));
+        }
+
+        // Shuffle the deck
+        Collections.shuffle(deck);
+
+        // Create the players
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player("Abdul"));
+        players.add(new Player("Sara"));
+        players.add(new Player("Harbeet"));
+        players.add(new Player("Abhay"));
+
+        // Deal the cards to the players
+        for (int i = 0; i < 7; i++) {
+            for (Player player : players) {
+                Card card = deck.remove(0);
+                player.drawCard(card);
+            }
+        }
+
+        // Initialize the game state
+        Card topCard = deck.remove(0);
+        UnoGame game = new UnoGame(players, topCard, deck);
+
+        // Start the game
+        game.playGame();
     }
-
-    
-     //return the name
-     
-    public String getName() {
-        return name;
-    }
-
-    
-      //return the players of this game
-     
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
-
-    
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
-
-  
-    public abstract void play();
-
-    /*
-      When the game is over display a winning player.
-     */
-    public abstract void declareWinner();
-
 }
